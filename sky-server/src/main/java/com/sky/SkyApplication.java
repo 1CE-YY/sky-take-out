5ca2e64fee7eacee0220e3efc9039dc222ba7262
1CE-YY <1307792296@qq.com>
2025-04-10 17:46:02 +0800

    订单状态定时处理功能代码开发

--- sky-server/src/main/java/com/sky/SkyApplication.java
+++ sky-server/src/main/java/com/sky/SkyApplication.java
@@ -10,7 +10,8 @@ import org.springframework.transaction.annotation.EnableTransactionManagement;
 @SpringBootApplication
 @EnableTransactionManagement //开启注解方式的事务管理
 @Slf4j
-@EnableCaching //开启注解方式的事务管理
+@EnableCaching //开启缓存注解功能
+@EnableScheduling //开启任务调度
 public class SkyApplication {
     public static void main(String[] args) {
         SpringApplication.run(SkyApplication.class, args);
